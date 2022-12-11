3c61adaf4625ef38f52612ff84c4e4efa90ca0fe
maiduytrinh <maiduytrinh150320@gmail.com>
2022-12-11 12:26:45 +0700

    1

--- src/main/java/com/app/security/WebSecurityConfig.java
+++ src/main/java/com/app/security/WebSecurityConfig.java
@@ -59,8 +59,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf()
                     .disable()
                 .authorizeRequests()
-                    .antMatchers("/api/users/login").permitAll()// Cho phép tất cả mọi người truy cập vào 2 địa chỉ này
-                    .anyRequest().authenticated(); // Tất cả các request khác đều cần phải xác thực mới được truy cập
+                    .antMatchers("/api/users/login").permitAll();// Cho phép tất cả mọi người truy cập vào 2 địa chỉ này
+                    // .anyRequest().authenticated(); // Tất cả các request khác đều cần phải xác thực mới được truy cập
 
         // Thêm một lớp Filter kiểm tra jwt
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
