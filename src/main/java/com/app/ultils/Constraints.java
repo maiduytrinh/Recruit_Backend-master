498f8db6432532f7d8bd0aad7417a5f1736b5981
maiduytrinh <maiduytrinh150320@gmail.com>
2022-12-12 09:46:12 +0700

    1

--- src/main/java/com/app/ultils/Constraints.java
+++ src/main/java/com/app/ultils/Constraints.java
@@ -5,7 +5,7 @@ public class Constraints {
     public static final String PATH_SERVER = "http://localhost:8080/";
     public static final String PATH_DOWNLOAD = "http://localhost:8080/download/";
     public static final String PATH_UPLOAD_MP3 = "http://localhost:8080/source_mp3/";
-    public static final String PATH_UPLOAD_IMAGE = "https://app-recruit-backend.herokuapp.com/image/";
+    public static final String PATH_UPLOAD_IMAGE = "https://recruit-app-backend.herokuapp.com/image/";
     public static final String ROLE_ADMIN = "ADMIN";
     public static final String ROLE_USER = "USER";
     public static final int ROLE_USER_ID = 2;
